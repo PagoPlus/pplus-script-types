24a357041672e4e5f6ba94ac49a7a8a7d23a1ec4
Fernando Mumbach <fermuch@ayvu.net>
2024-04-29 18:11:18 -0300

    FIX: formatting and renaming delete to del, since delete is a reserved word

--- modules/http.d.ts
+++ modules/http.d.ts
@@ -3,94 +3,118 @@
 import type { JSONValue } from "../utils"
 
 declare global {
-    namespace http {
-        /**
-         * Performs a GET request.
-         * 
-         * Returns `true` if the request succeeded, and the 
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-        function get(url: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
-    
-        /**
-         * Performs a POST request.
-         * 
-         * Returns `true` if the request succeeded, and the
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param body The body of the request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-        function post(url: string, body: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+  namespace http {
+    /**
+     * Performs a GET request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function get(
+      url: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
 
-        /**
-         * Performs a PUT request.
-         * 
-         * Returns `true` if the request succeeded, and the
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param body The body of the request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-         function put(url: string, body: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+    /**
+     * Performs a POST request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param body The body of the request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function post(
+      url: string,
+      body: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
 
-        /**
-         * Performs a DELETE request.
-         * 
-         * Returns `true` if the request succeeded, and the
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-        function delete(url: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+    /**
+     * Performs a PUT request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param body The body of the request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function put(
+      url: string,
+      body: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
 
-        /**
-         * Performs a PATCH request.
-         * 
-         * Returns `true` if the request succeeded, and the
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param body The body of the request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-        function patch(url: string, body: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+    /**
+     * Performs a DELETE request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function del(
+      url: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
 
-        /**
-         * Performs a HEAD request.
-         * 
-         * Returns `true` if the request succeeded, and the
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-        function head(url: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+    /**
+     * Performs a PATCH request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param body The body of the request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function patch(
+      url: string,
+      body: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
 
-        /**
-         * Performs a OPTIONS request.
-         * 
-         * Returns `true` if the request succeeded, and the
-         * second value is the response data.
-         * 
-         * @param url The URL to request.
-         * @param opts Additional options for the request.
-         * @tupleReturn
-         */
-        function options(url: string, opts?: { query?: Record<string, string>, headers?: Record<string, string> }): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
-      }
+    /**
+     * Performs a HEAD request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function head(
+      url: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+
+    /**
+     * Performs a OPTIONS request.
+     *
+     * Returns `true` if the request succeeded, and the
+     * second value is the response data.
+     *
+     * @param url The URL to request.
+     * @param opts Additional options for the request.
+     * @tupleReturn
+     */
+    function options(
+      url: string,
+      opts?: { query?: Record<string, string>; headers?: Record<string, string> },
+    ): LuaMultiReturn<[true, JSONValue]> | LuaMultiReturn<[false, string]>
+  }
 }
 
 export {}
